c56fabd11b92338e6ea7dda67fdccb93f98deea9
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-22 12:56:02 -0300

    fix: drop and recreate Author and Book models to fix author_id field

--- db/migrate/20240822155022_create_books.rb
+++ db/migrate/20240822155022_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[7.2]
   def change
     create_table :books do |t|
       t.string :name
-      t.references :Author, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_22_153159) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_22_155022) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -19,11 +19,11 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_22_153159) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
-    t.integer "Author_id", null: false
+    t.integer "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["Author_id"], name: "index_books_on_Author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
-  add_foreign_key "books", "Authors"
+  add_foreign_key "books", "authors"
 end
