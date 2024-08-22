475fc6a96ec622a221d4692ee0ed163a36b6095b
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-22 13:11:29 -0300

    feat: create new Account model and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_22_155022) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_22_160913) do
+  create_table "accounts", force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_accounts_on_author_id"
+  end
+
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -25,5 +32,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_22_155022) do
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  add_foreign_key "accounts", "authors"
   add_foreign_key "books", "authors"
 end
